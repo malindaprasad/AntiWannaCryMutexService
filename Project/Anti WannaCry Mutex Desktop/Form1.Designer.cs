0b4f97d0d67f402f8326258ad55c7b3bda0d72cf
Malinda <malinda555@gmail.com>
2017-05-14 01:56:10 +0530

    Desktop app

---
+++ Project/Anti WannaCry Mutex Desktop/Form1.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace Anti_WannaCry_Mutex_Desktop
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.SuspendLayout();
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.BalloonTipText = "Anti WannaCry Mutex";
+            this.notifyIcon1.BalloonTipTitle = "Anti WannaCry Mutex";
+            this.notifyIcon1.Text = "Anti WannaCry Mutex";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDoubleClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(263, 64);
+            this.ControlBox = false;
+            this.Name = "Form1";
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Anti WannaCry Mutex Desktop";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+    }
+}
+
---
+++ Project/Anti WannaCry Mutex Desktop/Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+/*
+
+    Created by : Malinda Rathnayake
+    Consultened by : Dilan Walgampaya
+    Date : 2017-05-13
+    Version : v01.0
+
+    Verify : Run https://technet.microsoft.com/en-us/sysinternals/processexplorer.aspx and search for "MsWinZonesCacheCounterMutexA"
+
+    Ref : https://gist.github.com/N3mes1s/afda0da98f6a0c63ec4a3d296d399636
+    */
+
+namespace Anti_WannaCry_Mutex_Desktop
+{
+    public partial class Form1 : Form
+    {
+        public static Mutex _mutey = null;
+        private bool allowVisible;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        protected override void SetVisibleCore(bool value)
+        {
+            if (!allowVisible)
+            {
+                value = false;
+                if (!this.IsHandleCreated) CreateHandle();
+            }
+            base.SetVisibleCore(value);
+            notifyIcon1.Visible = true;
+            init();
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Hide();
+            this.Visible = false;
+            notifyIcon1.Visible = true;
+            init();
+        }
+
+        public void init()
+        {
+            try
+            {
+
+                bool createdNew;
+
+                _mutey = new Mutex(false, @"Global\MsWinZonesCacheCounterMutexA", out createdNew);
+                _mutey.InitializeLifetimeService();
+
+
+            }
+            catch
+            {
+
+
+            }
+            analytics();
+        }
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+
+        }
+
+        public void analytics()
+        {
+            try
+            {
+                String s = System.Net.Dns.GetHostName();
+                String address = "http://apps.malidaprasad.com/report/?app=AntiWannaCryMutex&ver=v1.0&machine=" + s;
+                using (WebClient client = new WebClient())
+                {
+                    client.DownloadString(address);
+                }
+            }
+            catch (Exception e)
+            {
+
+            }
+
+
+        }
+
+       
+    }
+}
+
